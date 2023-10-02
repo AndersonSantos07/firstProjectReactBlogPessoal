6719c2732d8ac3c8cd9b58a865735ef6c518bc46
Anderson <andersonfullstackdevoloper@gmail.com>
2023-10-02 10:01:43 -0300

    react-app

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './Home.css'
+import homeLogo from '../../assets/home.jpg'
+
+function Home() {
+
+    return (
+
+        <>
+
+            <h1 className="titulo">Home</h1>
+            <img src={homeLogo} className="img" alt="Imagem Tela Inicial" />
+
+        </>
+    )
+}
+
+export default Home;
